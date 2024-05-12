2dab61248c3f519ddc9b9d870f07303df1d2d9f2
Gilles Coissac <gilles@ideographe.fr>
2024-05-12 20:10:38 +0200

    fix: fix module import

--- local_extensions/__init__.py
+++ local_extensions/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from python_version_extension import PythonVersionExtension
+from .python_version_extension import PythonVersionExtension
 
 
 __all__ = ["PythonVersionExtension"]
