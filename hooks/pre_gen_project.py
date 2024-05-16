c03ea2230b2b10f0387ea0342b8adf986b7ca92b
Gilles Coissac <gilles@ideographe.fr>
2024-05-16 17:58:23 +0200

    fix: fix name normalization and validation

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,16 +6,19 @@ import re
 import sys
 from typing import Pattern
 
-
-# Project names must be letters, numbers, dashes,
-# or spaces, but not start with a space or dash.
-VALID_PROJECT: Pattern[str] = re.compile(
-    r"^([A-Z0-9]|[A-Z0-9][A-Z0-9- ]*[A-Z0-9])$", re.IGNORECASE
+# A valid name consists only of ASCII letters 
+# and numbers, period, underscore and hyphen. 
+# It must start and end with a letter or number.
+# https://packaging.python.org/en/latest/specifications/name-normalization/ 
+VALID_PROJECT_NAME: Pattern[str] = re.compile(
+    r"^([A-Z0-9]|[A-Z0-9][A-Z0-9._-]*[A-Z0-9])$", re.IGNORECASE
 )
+
 
 # Repository names must be lower-case letters,
 # numbers, or underscores, but not start with an underscore.
-VALID_REPO: Pattern[str] = re.compile(r"^([a-z0-9]|[a-z0-9][a-z0-9_]*[a-z0-9])$")
+VALID_REPO_NAME: Pattern[str] = re.compile(r"^([a-z0-9]|[a-z0-9][a-z0-9_]*[a-z0-9])$")
+
 
 # PYTHON VERSION SPECIFICATION
 # FIXME: do not handle 3.* or 3.8.*
@@ -27,7 +30,7 @@ PYTHON_VERSION = (
     r"(?<version>(?P<release>(?P<major>[0-9]+)(?:(?:\.)(?P<minor>[0-9]+))?"
     r"(?:(?:\.)(?P<micro>[0-9]+))?)(?P<pre>(?:a|b|rc|alpha|beta)(?:[-_\.]?[0-9]+)?)?)"
 )
-VERSION_PYTHON_SPECIFIER: Pattern[str] = re.compile(
+VALID_PYTHON_VERSION_SPECIFIER: Pattern[str] = re.compile(
     r"^(~=|==|!=|<=|>=|<|>|===)((([0-9]+)(?:(?:\.)([0-9]+))?(?:(?:\.)([0-9]+))?)((?:a|b|rc|alpha|beta)(?:[-_\.]?[0-9]+)?)?)(,\s*(~=|==|!=|<=|>=|<|>|===)((([0-9]+)(?:(?:\.)([0-9]+))?(?:(?:\.)([0-9]+))?)((?:a|b|rc|alpha|beta)(?:[-_\.]?[0-9]+)?)?))*$"
 )
 
@@ -44,7 +47,7 @@ def validate_text(text: str, regex: Pattern[str], error_label: str) -> None:
         ValueError: if "text" does not match "regex".
     """
     if not text or regex.fullmatch(text) is None:
-        message = f"The project name {text} is not a valid {error_label}"
+        message = f"Input <{text}> is not a valid {error_label}"
         raise ValueError(message)
 
 
@@ -52,19 +55,19 @@ def main() -> None:
     """Calls validation functions."""
     validate_text(
         text="{{ cookiecutter.project_name }}",
-        regex=VALID_PROJECT,
+        regex=VALID_PROJECT_NAME,
         error_label="project name",
     )
 
     validate_text(
         text="{{ cookiecutter.repo_name }}",
-        regex=VALID_REPO,
+        regex=VALID_REPO_NAME,
         error_label="repository name",
     )
 
     validate_text(
         text="{{ cookiecutter.python }}",
-        regex=VERSION_PYTHON_SPECIFIER,
+        regex=VALID_PYTHON_VERSION_SPECIFIER,
         error_label="python version specifiers set",
     )
 
